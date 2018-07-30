59603b04beaeaf1c902822265a544cf1e2331fe0
TylerWasniowski <TylerWasniowski@gmail.com>
2018-07-30 13:59:08 -0700

    Organized requests into modules

--- data/tutors.js
+++
@@ -1,9 +0,0 @@
-var tutors = [
-    {
-        name: "John Doe",
-        email: "JohnDoe@gmail.com"
-        setmoreId: "XXXXXXXXXXXXXXX",
-        timeclockId: "XXXXXX"
-    }
-];
-module.exports = tutors;
--- public/javascripts/indexSetup.js
+++ public/javascripts/indexSetup.js
@@ -1,23 +1,23 @@
+const PAY_PERIOD_START_INPUT_SELECTOR = '#payPeriodStartInput';
+const PAY_PERIOD_END_INPUT_SELECTOR = '#payPeriodEndInput';
+
 const HOURS_FILE_BUTTON_SELECTOR = '#hoursFileButton';
+const UPDATE_TUTORS_BUTTON_SELECTOR = '#updateTutorsButton';
 
-// Function taken from: https://stackoverflow.com/a/18197341
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-}
+let payPeriodStartInput = document.querySelector(PAY_PERIOD_START_INPUT_SELECTOR);
+let payPeriodEndInput = document.querySelector(PAY_PERIOD_END_INPUT_SELECTOR);
 
 let hoursFileButton = document.querySelector(HOURS_FILE_BUTTON_SELECTOR);
+let updateTutorsButton = document.querySelector(UPDATE_TUTORS_BUTTON_SELECTOR);
+
 
 hoursFileButton.onclick = () => {
-    fetch('/data/hours')
+    var uri = '/data/hours?' +
+    'payPeriodStart=' + payPeriodStartInput.value +
+    '&payPeriodEnd=' + payPeriodEndInput.value;
+    
+    fetch(uri)
         .then((res) => res.json())
         .then((res) => res
             .map((entry) => {
@@ -33,3 +33,25 @@ hoursFileButton.onclick = () => {
         .then((res) => download('hours.txt', res));
     return false;
 };
+
+updateTutorsButton.onclick = () => {
+    fetch('/tutors/update', {method: 'POST'})
+        .then((res) => res.json())
+        .then((res) => download('tutors.json', JSON.stringify(res)));
+
+    return false;
+}
+
+// Taken from: https://stackoverflow.com/a/18197341
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
