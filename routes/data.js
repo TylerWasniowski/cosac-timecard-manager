b325c109fac444a85b7117a706d07c7c12b2bad9
TylerWasniowski <TylerWasniowski@gmail.com>
2018-08-12 19:19:57 -0700

    Implemented extended hours

--- routes/data.js
+++ routes/data.js
@@ -35,7 +35,7 @@ async function getHours(payPeriodStart, payPeriodEnd) {
                 responses[1].find((entry) => entry.ResourceKey == tutor.setmoreId),     
                 responses[2].filter((appointment) => appointment.serviceKey == 'slotBlocker')
             ])
-            .then((responses) => evaluateHoursWorked(tutor, payPeriodStartObj, payPeriodEndObj,
+            .then((responses) => evaluateHoursWorked(tutor.name, payPeriodStartObj, payPeriodEndObj,
                 responses[0], responses[1], responses[2])
             );
     });
@@ -43,71 +43,111 @@ async function getHours(payPeriodStart, payPeriodEnd) {
     return await Promise.all(staffHoursPromises);
 }
 
-function evaluateHoursWorked(tutor, payPeriodStartObj, payPeriodEndObj,
+function evaluateHoursWorked(name, payPeriodStartObj, payPeriodEndObj,
     openTimeClockHours, setmoreHours, slotBlockers) {
-    console.log(tutor.name);
+    console.log(name);
     console.log(++tutorsDone + '/' + tutors.length);
     if (!setmoreHours) {
         return {
-            name: tutor.name + ': ' + 'Setmore Error',
+            name: name + ': ' + 'Setmore Error',
             hours: {}
         }
     }
 
-    // Transform OpenTimeClock data to this format:
-    /*
-    {
-        'xxxxxxx': {
-            'MM/DD/YYYY': {
-                onIntervals: [{startObj: momentObj, endObj: momentObj}, ...]
-                offIntervals: [{startObj: momentObj, endObj: momentObj}, ...]
-            },
-            ...
-        },
-        ...
-    }
-    */
-    var datesToIntervals = {};
-    // Populate all the dates from the start to the end (inclusive) with empty interval objects
-    for (var dateObj = moment(payPeriodStartObj); dateObj.isSameOrBefore(payPeriodEndObj); dateObj.add(1, 'd')) {
-        datesToIntervals[dateObj.format(config.dateFormat)] = {
-            onIntervals: [],
-            offIntervals: []
-        };
-    }
-    openTimeClockHours.forEach((entry) => {
-        var startDateTimeObj = moment(entry.InDateTime, config.openTimeClockDateTimeFormat);
-        var endDateTimeObj = moment(entry.OutDateTime, config.openTimeClockDateTimeFormat);
+    var openTimeClockDatesToIntervals = formattedOpenTimeClockHours(openTimeClockHours, payPeriodStartObj, payPeriodEndObj);
+    var setmoreDatesToIntervals = formattedSetmoreHours(setmoreHours, payPeriodStartObj, payPeriodEndObj);
 
-        // console.log(entry.InDateTime);
-        // console.log(startDateTimeObj.format(config.dateFormat));
-        // console.log(datesToIntervals);
-        // Object.keys(datesToIntervals)
-        //     .forEach(x => console.log(x + '| ' + x == startDateTimeObj.format(config.dateFormat)));
-        // console.log(Object.keys(datesToIntervals));
-        // console.log('test');
+    // Add extended hours
+    const extendedEntries = slotBlockers
+    .filter((slotBlocker) => slotBlocker.Comments.startsWith('E -'));
 
-        var intervalsObj = datesToIntervals[startDateTimeObj.format(config.dateFormat)];
+    extendedEntries.forEach((entry) => {
+        const durationMinutes = entry.endTime - entry.startTime;
+        const startObj = moment(entry.startDate, config.setmoreResponseDateTimeFormat);
+        const endObj = moment(startObj).add(durationMinutes, 'minutes');
 
-        var intervalPair = Array.from(intervalsObj.onIntervals.entries())
-            .find((intervalPair) => startDateTimeObj.isAfter(intervalPair[1]));
-        var insertIndex = intervalPair ? intervalPair[0] : 0;
-
-        var intervalObj = {
-            startObj: startDateTimeObj,
-            endObj: endDateTimeObj
-        };
-        intervalsObj.onIntervals.splice(insertIndex, 0, intervalObj);
+        const interval = { startObj, endObj };
+        insertInterval(openTimeClockDatesToIntervals, interval);                
     });
+    
 
-    // Transform Setmore data to this format:
-    /*
-    {
-        sunday: {startObj: momentObj, endObj: momentObj},
-        ...,
-        saturday: {startObj: momentObj, endObj, momentObj}
+    // Only accept the intervals that are logged both on Setmore and OpenTimeClock
+    var datesToIntervals = datesToIntervalsPopulated(payPeriodStartObj, payPeriodEndObj);
+    Object
+        .keys(datesToIntervals)
+        .forEach((date) => {
+            datesToIntervals[date] = intervalsIntersection(
+                openTimeClockDatesToIntervals[date],
+                setmoreDatesToIntervals[date]
+            );
+        });
+
+    // Handle breaks
+
+    // Handle time off
+
+    // Add admin hours
+    const adminEntries = slotBlockers
+        .filter((slotBlocker) => slotBlocker.Comments.startsWith('A -'));
+
+        adminEntries.forEach((entry) => {
+        const durationMinutes = entry.endTime - entry.startTime;
+        const startObj = moment(entry.startDate, config.setmoreResponseDateTimeFormat);
+        const endObj = moment(startObj).add(durationMinutes, 'minutes');
+
+        const interval = { startObj, endObj };
+        insertInterval(openTimeClockDatesToIntervals, interval);                
+    });
+    
+    // Transform intervals to hours worked
+    var datesToHoursWorked = {};
+    for (var date in openTimeClockDatesToIntervals) {
+        if (openTimeClockDatesToIntervals.hasOwnProperty(date)) {
+            datesToHoursWorked[date] = openTimeClockDatesToIntervals[date]
+                .reduce((hours, intervalsObj) => {
+                    return hours + intervalsObj.endObj.diff(intervalsObj.startObj, 'hours', true);
+                }, 0);
+        }
     }
-    */
+    
+    return {
+        name,
+        hours: datesToHoursWorked
+    };
+}
+
+// Transform OpenTimeClock data to this format:
+/*
+{
+    'MM/DD/YYYY': [{startObj: momentObj, endObj: momentObj}, ...],
+    ...
+}
+*/
+function formattedOpenTimeClockHours(openTimeClockHours, startDateObj, endDateObj) {
+   var datesToIntervals = datesToIntervalsPopulated(startDateObj, endDateObj);
+
+   openTimeClockHours.forEach((entry) => {
+       var startDateTimeObj = moment(entry.InDateTime, config.openTimeClockDateTimeFormat);
+       var endDateTimeObj = moment(entry.OutDateTime, config.openTimeClockDateTimeFormat);
+
+       const interval = {
+           startObj: startDateTimeObj,
+           endObj: endDateTimeObj
+       };
+       insertInterval(datesToIntervals, interval, true);
+   });
+
+   return datesToIntervals;
+}
+
+// Transform Setmore data to this format:
+/*
+{
+    'MM/DD/YYYY': [{startObj: momentObj, endObj: momentObj}, ...],
+    ...
+}
+*/
+function formattedSetmoreHours(setmoreHours, startDateObj, endDateObj) {
     var setmoreDayToHours = {
         sunday: setmoreHours.su,
         monday: setmoreHours.mo,
@@ -127,78 +167,28 @@ function evaluateHoursWorked(tutor, payPeriodStartObj, payPeriodEndObj,
             };
         });
 
-    // Disregard time clocked in but not open on Setmore
-    for (var date in datesToIntervals) {
-        if (datesToIntervals.hasOwnProperty(date)) {
-            var dateObj = moment(date, config.dateFormat);
-            var day = dateObj
+    var datesToIntervals = datesToIntervalsPopulated(startDateObj, endDateObj);
+    Object
+        .keys(datesToIntervals)
+        .forEach((date) => {
+            const day = moment(date, config.dateFormat)
                 .format('dddd')
                 .toLowerCase();
-            var dayHoursData = setmoreDayToHours[day];
-            
-            if (datesToIntervals[date].onIntervals.length > 0) {
-                var firstInterval = datesToIntervals[date].onIntervals[0];
-                var lastInterval = datesToIntervals[date].onIntervals[
-                    datesToIntervals[date].onIntervals.length - 1
-                ];
+                
+            const interval = setmoreDayToHours[day];
+            datesToIntervals[date].push(interval);
+        });
+    return datesToIntervals;
+}
 
-                var setmoreStartObj = moment(firstInterval.startObj)
-                    .hours(dayHoursData.startObj.hours())
-                    .minutes(dayHoursData.startObj.minutes());
-                var setmoreEndObj = moment(firstInterval.endObj)
-                    .hours(dayHoursData.endObj.hours())
-                    .minutes(dayHoursData.endObj.minutes());
-                firstInterval.startObj = moment.max(setmoreStartObj, firstInterval.startObj);
-                lastInterval.endObj = moment.min(setmoreEndObj, lastInterval.endObj);        
-            }
-            datesToIntervals[date].onIntervals = simplifiedIntervals(datesToIntervals[date].onIntervals);
-        }
-    }
+function mergeDatesToIntervals(a, b) {
 
-    // Handle breaks
-
-    // Handle time off
-
-
-    // Add extended hours
-    const extendedEntries = slotBlockers.filter((slotBlocker) => slotBlocker.comments.startsWith('E -'));
-    extendedEntries.forEach((entry) => {
-        const durationMinutes = entry.endTime - entry.startTime;
-
-        const interval = {
-            startObj
-        }
-    })
-
-    // Add admin hours
-    const adminEntries = slotBlockers.filter((slotBlocker) => slotBlocker.comments.startsWith('A -'));
-
-    
-    // Transform intervals to hours worked
-    var datesToHoursWorked = {};
-    for (var date in datesToIntervals) {
-        if (datesToIntervals.hasOwnProperty(date)) {
-            datesToHoursWorked[date] = datesToIntervals[date]
-                .onIntervals.reduce((hours, intervalsObj) => {
-                    return hours + intervalsObj.endObj.diff(intervalsObj.startObj, 'h', true);
-                }, 0);
-        }
-    }
-    
-    return {
-        name: tutor.name,
-        hours: datesToHoursWorked
-    };
 }
 
 // Repeatedly merges intervals until array is simplified
 // Array must be sorted by startObjs
-function simplifiedIntervals(intervals) {
-    var copy = intervals.map(interval => {
-        return {
-            startObj: moment(interval.startObj),
-            endObj: moment(interval.endObj)
-    };});
+function intervalsSimple(intervals) {
+    var copy = intervalsCopied(intervals);
     for (var i = 0; i < copy.length - 1; i++) {
         if (copy[i + 1].startObj.isSameOrBefore(copy[i].endObj)) {
             // Merge the two intervals
@@ -210,39 +200,38 @@ function simplifiedIntervals(intervals) {
     return copy.filter((interval) => interval.startObj.isBefore(interval.endObj));
 }
 
-function findIntervalsDifference(a, b) {
-    var difference = a.map(interval => {
-        return {
-            startObj: moment(interval.startObj),
-            endObj: moment(interval.endObj)
-    };});
+function intervalsDifference(a, b) {
+    var difference = intervalsCopied(a);
     var i = 0;
     var j = 0;
     while (i < difference.length && j < b.length) {
-        if (difference[i].startObj.isBefore(b[j].endObj) && difference[i].endObj.isAfter(b[j].startObj)) {
-            if (difference[i].startObj.isSameOrAfter(b[j].startObj)) {
-                difference[i] = {
-                    startObj: b[j].endObj,
-                    endObj: difference[i].startObj
-                };
-            }
-            if (difference[i].endObj.isSameOrBefore(b[j].endObj)) {
-                difference[i] = {
-                    startObj: difference[i].startObj,
-                    endObj: b[j].startObj
-                };
-            }
-            if (difference[i].startObj.isBefore(b[j].startObj) && difference[i].endObj.isAfter(b[j].endObj)) {
+        if (areOverlapping(difference[i], b[j])) {
+            if (isContained(difference[i], b[j])) {
                 // Split interval into two
                 difference.splice(i + 1, 0, {
-                    startObj: b[j].endObj, 
+                    startObj: moment(b[j].endObj), 
                     endObj: difference[i].endObj
                 });
                 difference[i] = {
                     startObj: difference[i].startObj,
-                    endObj: b[j].startObj
+                    endObj: moment(b[j].startObj)
                 };
                 i++;
+                j++;
+            } else {
+                if (difference[i].endObj.isSameOrBefore(b[j].endObj)) {
+                    difference[i] = {
+                        startObj: difference[i].startObj,
+                        endObj: moment(b[j].startObj)
+                    };
+                }
+                if (difference[i].startObj.isSameOrAfter(b[j].startObj)) {
+                    difference[i] = {
+                        startObj: moment(b[j].endObj),
+                        endObj: difference[i].endObj
+                    };
+                }
+                j++;
             }
         } else if (difference[i].endObj.isSameOrBefore(b[j].startObj)) {
             i++;
@@ -253,6 +242,98 @@ function findIntervalsDifference(a, b) {
 
     // Return only non-empty intervals
     return difference.filter(interval => interval.startObj.isBefore(interval.endObj));
+}
+
+function intervalsIntersection(a, b) {
+    var intersection = intervalsCopied(a);
+    var i = 0;
+    var j = 0;
+    while (i < intersection.length && j < b.length) {
+        if (areOverlapping(intersection[i], b[j])) {
+            if (isContained(intersection[i], b[j])) {
+                // Split interval into two
+                intersection.splice(i + 1, 0, {
+                    startObj: moment(b[j].endObj), 
+                    endObj: intersection[i].endObj
+                });
+                intersection[i] = {
+                    startObj: moment(b[j].startObj),
+                    endObj: moment(b[j].endObj)
+                };
+                i++;
+                j++;
+            } else {
+                if (!isContained(b[j], intersection[i])) {
+                    if (intersection[i].startObj.isBefore(b[j].startObj)) {
+                        intersection[i] = {
+                            startObj: moment(b[j].startObj),
+                            endObj: intersection[i].endObj
+                        };
+                    } else {
+                        // Split interval into two
+                        intersection.splice(i + 1, 0, {
+                            startObj: moment(b[j].endObj), 
+                            endObj: intersection[i].endObj
+                        });
+                        intersection[i] = {
+                            startObj: intersection[i].startObj,
+                            endObj: moment(b[j].endObj)
+                        };
+                    }
+                }
+                i++;
+            }
+        } else if (intersection[i].endObj.isSameOrBefore(b[j].startObj)) {
+            intersection.splice(i, 1);
+        } else {
+            j++;
+        }
+    }
+    
+    return intersection.slice(0, i);
+}
+
+function intervalsCopied(intervals) {
+    return intervals.map((interval) => {
+        return {
+            startObj: moment(interval.startObj),
+            endObj: moment(interval.endObj)
+    };});
+}
+
+
+function insertInterval(datesToIntervals, interval) {
+    var intervals = datesToIntervals[interval.startObj.format(config.dateFormat)];
+    var insertIndex = findInsertIndex(intervals, interval.startObj);
+    
+    intervals.splice(insertIndex, 0, interval);
+}
+
+function findInsertIndex(intervals, timeObj) {
+    var intervalPair = Array.from(intervals.entries())
+        .find((intervalPair) => timeObj.isAfter(intervalPair[1]));
+
+    return intervalPair ? intervalPair[0] : 0;
+}
+
+// Populate all the dates from the start to the end (inclusive) with empty interval objects
+function datesToIntervalsPopulated(startDateObj, endDateObj) {
+    var datesToIntervals = {};
+    for (var dateObj = moment(startDateObj); dateObj.isSameOrBefore(endDateObj); dateObj.add(1, 'day')) {
+        datesToIntervals[dateObj.format(config.dateFormat)] = [];
+    }
+
+    return datesToIntervals;
+}
+
+// Returns true if interval-a overlaps at all with interval b and false otherwise
+function areOverlapping(a, b) {
+    return a.startObj.isSameOrBefore(b.endObj) && a.endObj.isSameOrAfter(b.startObj);
+}
+
+// Returns true if interval-b is contained within interval-a
+function isContained(a, b) {
+    return a.startObj.isBefore(b.startObj) && a.endObj.isAfter(b.endObj);
 }
 
 module.exports = router;
