21eb6972ddd8282648c065efc27939625b7c2740
TylerWasniowski <TylerWasniowski@gmail.com>
2018-08-14 14:04:30 -0700

    Fixed scoping bug

--- public/javascripts/blockersOptions.js
+++ public/javascripts/blockersOptions.js
@@ -1,57 +1,59 @@
-let TIME_START_SELECTOR = '#timeStart';
-let TIME_END_SELECTOR = '#timeEnd';
-let DESCRIPTION_SELECTOR = '#description';
-let DATES_CONTAINER_SELECTOR = '.dates';
-let DATE_SELECTOR = DATES_CONTAINER_SELECTOR + ' input[type=\'date\']';
-let TUTOR_CHECKBOX_SELECTOR = '.tutors input[type=\'checkbox\']';
-let CREATE_SLOT_BLOCKERS_BUTTON_SELECTOR = '#createSlotBlockersButton';
+(() => {
+    const TIME_START_SELECTOR = '#timeStart';
+    const TIME_END_SELECTOR = '#timeEnd';
+    const DESCRIPTION_SELECTOR = '#description';
+    const DATES_CONTAINER_SELECTOR = '.dates';
+    const DATE_SELECTOR = DATES_CONTAINER_SELECTOR + ' input[type=\'date\']';
+    const TUTOR_CHECKBOX_SELECTOR = '.tutors input[type=\'checkbox\']';
+    const CREATE_SLOT_BLOCKERS_BUTTON_SELECTOR = '#createSlotBlockersButton';
 
-let datesContainer = document.querySelector(DATES_CONTAINER_SELECTOR);
-let firstDateInput = document.querySelector(DATE_SELECTOR);
-let createSlotBlockersButton = document.querySelector(CREATE_SLOT_BLOCKERS_BUTTON_SELECTOR);
+    const datesContainer = document.querySelector(DATES_CONTAINER_SELECTOR);
+    const firstDateInput = document.querySelector(DATE_SELECTOR);
+    const createSlotBlockersButton = document.querySelector(CREATE_SLOT_BLOCKERS_BUTTON_SELECTOR);
 
-firstDateInput.onchange = () => {
-    addDateInput();
-    firstDateInput.onchange = () => {};
-};
-
-createSlotBlockersButton.onclick = () => {
-    fetch('/blockers/create', {
-        method: 'POST',
-        body: JSON.stringify({
-            timeStart: document.querySelector(TIME_START_SELECTOR).value,
-            timeEnd: document.querySelector(TIME_END_SELECTOR).value,
-            description: document.querySelector(DESCRIPTION_SELECTOR).value,
-            dates: getSelectedDates(),
-            tutors: getCheckedTutors()
-        }),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    });
-}
-
-function addDateInput() {
-
-    datesContainer.appendChild(document.createElement('br'));
-
-    const dateInput = document.createElement('input');
-    dateInput.type = 'date';
-    dateInput.onchange = () => {
+    firstDateInput.onchange = () => {
         addDateInput();
-        dateInput.onchange = () => {};
+        firstDateInput.onchange = () => {};
     };
-    datesContainer.appendChild(dateInput);
-}
 
-function getSelectedDates() {
-    return Array.from(document.querySelectorAll(DATE_SELECTOR))
-        .filter((date) => date.value)
-        .map((date) => date.value);
-}
+    createSlotBlockersButton.onclick = () => {
+        fetch('/blockers/create', {
+            method: 'POST',
+            body: JSON.stringify({
+                timeStart: document.querySelector(TIME_START_SELECTOR).value,
+                timeEnd: document.querySelector(TIME_END_SELECTOR).value,
+                description: document.querySelector(DESCRIPTION_SELECTOR).value,
+                dates: getSelectedDates(),
+                tutors: getCheckedTutors()
+            }),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+    }
 
-function getCheckedTutors() {
-    return Array.from(document.querySelectorAll(TUTOR_CHECKBOX_SELECTOR))
-        .filter((checkbox) => checkbox.checked)
-        .map((checkbox) => checkbox.id);
-}
+    function addDateInput() {
+
+        datesContainer.appendChild(document.createElement('br'));
+
+        const dateInput = document.createElement('input');
+        dateInput.type = 'date';
+        dateInput.onchange = () => {
+            addDateInput();
+            dateInput.onchange = () => {};
+        };
+        datesContainer.appendChild(dateInput);
+    }
+
+    function getSelectedDates() {
+        return Array.from(document.querySelectorAll(DATE_SELECTOR))
+            .filter((date) => date.value)
+            .map((date) => date.value);
+    }
+
+    function getCheckedTutors() {
+        return Array.from(document.querySelectorAll(TUTOR_CHECKBOX_SELECTOR))
+            .filter((checkbox) => checkbox.checked)
+            .map((checkbox) => checkbox.id);
+    }
+})();
--- public/javascripts/hoursOptions.js
+++ public/javascripts/hoursOptions.js
@@ -1,62 +1,63 @@
-let REQUEST_HOURS_BUTTON_SELECTOR = '#requestHoursButton';
+(() => {
+    const REQUEST_HOURS_BUTTON_SELECTOR = '#requestHoursButton';
+    const PAY_PERIOD_START_INPUT_SELECTOR = '#payPeriodStartInput';
+    const PAY_PERIOD_END_INPUT_SELECTOR = '#payPeriodEndInput';
+    const TUTOR_CHECKBOX_SELECTOR = '.tutors input[type=\'checkbox\']';
 
-let PAY_PERIOD_START_INPUT_SELECTOR = '#payPeriodStartInput';
-let PAY_PERIOD_END_INPUT_SELECTOR = '#payPeriodEndInput';
-let TUTOR_CHECKBOX_SELECTOR = '.tutors input[type=\'checkbox\']';
+    const payPeriodStartInput = document.querySelector(PAY_PERIOD_START_INPUT_SELECTOR);
+    const payPeriodEndInput = document.querySelector(PAY_PERIOD_END_INPUT_SELECTOR);
 
-let payPeriodStartInput = document.querySelector(PAY_PERIOD_START_INPUT_SELECTOR);
-let payPeriodEndInput = document.querySelector(PAY_PERIOD_END_INPUT_SELECTOR);
+    const requestHoursButton = document.querySelector(REQUEST_HOURS_BUTTON_SELECTOR);
 
-let requestHoursButton = document.querySelector(REQUEST_HOURS_BUTTON_SELECTOR);
+    requestHoursButton.onclick = () => {
+        var uri = '/data/hours?' +
+        'payPeriodStart=' + payPeriodStartInput.value +
+        '&payPeriodEnd=' + payPeriodEndInput.value;
+        
+        fetch(uri, {
+            method: 'POST',
+            body: JSON.stringify({
+                tutors: getCheckedTutors()
+            }),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((res) => res
+                .map((entry) => {
+                    var hoursString = Object
+                        .keys(entry.hours)
+                        // Round hours to nearest 0.5
+                        .map((day) => day + ': ' + (Math.round(entry.hours[day] * 2) / 2))
+                        .join('\n');
 
-requestHoursButton.onclick = () => {
-    var uri = '/data/hours?' +
-    'payPeriodStart=' + payPeriodStartInput.value +
-    '&payPeriodEnd=' + payPeriodEndInput.value;
-    
-    fetch(uri, {
-        method: 'POST',
-        body: JSON.stringify({
-            tutors: getCheckedTutors()
-        }),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((res) => res.json())
-        .then((res) => res
-            .map((entry) => {
-                var hoursString = Object
-                    .keys(entry.hours)
-                    // Round hours to nearest 0.5
-                    .map((day) => day + ': ' + (Math.round(entry.hours[day] * 2) / 2))
-                    .join('\n');
+                    return entry.name + '\n' + hoursString;
+                })
+                .join('\n\n')
+            )
+            .then((res) => download('hours.txt', res));
 
-                return entry.name + '\n' + hoursString;
-            })
-            .join('\n\n')
-        )
-        .then((res) => download('hours.txt', res));
+        return false;
+    };
 
-    return false;
-};
+    function getCheckedTutors() {
+        return Array.from(document.querySelectorAll(TUTOR_CHECKBOX_SELECTOR))
+            .filter((checkbox) => checkbox.checked)
+            .map((checkbox) => checkbox.id);
+    }
 
-function getCheckedTutors() {
-    return Array.from(document.querySelectorAll(TUTOR_CHECKBOX_SELECTOR))
-        .filter((checkbox) => checkbox.checked)
-        .map((checkbox) => checkbox.id);
-}
+    // Taken from: https://stackoverflow.com/a/18197341
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
 
-// Taken from: https://stackoverflow.com/a/18197341
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+        element.click();
 
-    element.click();
-
-    document.body.removeChild(element);
-}
+        document.body.removeChild(element);
+    }
+})();
