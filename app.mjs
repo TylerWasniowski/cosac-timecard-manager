85041902c103990ccbac30ca0797bdeb165fa216
Amila Welihinda <amilajack@gmail.com>
2018-09-08 09:53:50 -0700

    bumped deps

--- app.mjs
+++ app.mjs
@@ -20,14 +20,14 @@ setmore.addSession();
 setmore.addSession();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve('./views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve('./public')));
 
 app.use('/', indexRouter);
 app.use('/data', dataRouter);
