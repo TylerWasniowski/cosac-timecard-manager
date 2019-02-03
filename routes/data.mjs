3e8a4333c9a8e9ae8a257f9efc660dc5789db884
TylerWasniowski <TylerWasniowski@gmail.com>
2019-02-02 16:20:32 -0800

    Formatted code

--- routes/data.mjs
+++ routes/data.mjs
@@ -4,16 +4,16 @@ import allTutors from '../data/tutors.json';
 
 import calculations from '../lib/calculations';
 
-
 const router = express.Router();
 
 router.post('/hours', async (req, res) => {
-  const tutors = req
-    .body
-    .tutors
-    .map(setmoreId => allTutors.find(tutor => tutor.setmoreId === setmoreId));
+  const tutors = req.body.tutors.map(setmoreId => allTutors.find(tutor => tutor.setmoreId === setmoreId));
 
-  const hours = await calculations.getHours(req.query.payPeriodStart, req.query.payPeriodEnd, tutors);
+  const hours = await calculations.getHours(
+    req.query.payPeriodStart,
+    req.query.payPeriodEnd,
+    tutors
+  );
   res.send(hours);
 });
 
